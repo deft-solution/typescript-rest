11e70e25c13e94d5b010c507d04227704f010ac0
YanPuty <yanputy123@gmail.com>
2024-03-24 15:56:28 +0700

    Add Express Server

---
+++ src/server.ts
@@ -0,0 +1,46 @@
+import 'reflect-metadata';
+
+import * as dotenv from 'dotenv';
+import express, { Application } from 'express';
+import morgan from 'morgan';
+import * as path from 'path';
+
+class Server {
+  private app: Application;
+
+  public static bootstrap(): Server {
+    return new Server();
+  }
+
+  constructor() {
+    this.app = express();
+    this._config();
+  }
+
+  private _config() {
+
+    dotenv.config();
+
+    // set port server
+    this.app.set("port", process.env.PORT || 3000);
+
+    // Morgan logging middleware
+    this.app.use(morgan('dev'));
+
+    // add static paths
+    this.app.use(express.static(path.join(__dirname, "public")));
+
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
+
+  public start() {
+    this.app.listen(this.app.get("port"), () => {
+      console.log(("App is running at http://localhost:%d in %s mode"), this.app.get("port"), this.app.get("env"));
+      console.log("Press CTRL-C to stop\n");
+    });
+  }
+}
+
+const server = new Server();
+server.start();
